f858467aacb708559ecb729cdcd3b4dff11b4bb2
Djordje_Pavlovic <pavlovicdjordje35@gmail.com>
2023-03-31 17:57:43 +0200

    push for protection

--- RISC_V/RISC_V.srcs/sources_1/new/forwarding_unit.vhd
+++ RISC_V/RISC_V.srcs/sources_1/new/forwarding_unit.vhd
@@ -1,43 +1,25 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/31/2023 05:48:07 PM
--- Design Name: 
--- Module Name: forwarding_unit - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity forwarding_unit is
---  Port ( );
-end forwarding_unit;
-
-architecture Behavioral of forwarding_unit is
-
-begin
-
-
-end Behavioral;
+port (
+-- ulazi iz ID faze
+rs1_address_id_i : in std_logic_vector(4 downto 0);
+rs2_address_id_i : in std_logic_vector(4 downto 0);
+-- ulazi iz EX faze
+rs1_address_ex_i : in std_logic_vector(4 downto 0);
+rs2_address_ex_i : in std_logic_vector(4 downto 0);
+-- ulazi iz MEM faze
+rd_we_mem_i : in std_logic;
+rd_address_mem_i : in std_logic_vector(4 downto 0);
+-- ulazi iz WB faze
+rd_we_wb_i : in std_logic;
+rd_address_wb_i : in std_logic_vector(4 downto 0);
+-- izlazi za prosledjivanje operanada ALU jedinici
+alu_forward_a_o : out std_logic_vector (1 downto 0);
+alu_forward_b_o : out std_logic_vector(1 downto 0);
+-- izlazi za prosledjivanje operanada komparatoru za odredjivanje uslovaskoka
+branch_forward_a_o : out std_logic;
+branch_forward_b_o : out std_logic);
+end entity;
--- RISC_V/RISC_V.srcs/sources_1/new/hazard_unit.vhd
+++ RISC_V/RISC_V.srcs/sources_1/new/hazard_unit.vhd
@@ -1,43 +1,26 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/31/2023 05:47:28 PM
--- Design Name: 
--- Module Name: hazard_unit - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 entity hazard_unit is
---  Port ( );
-end hazard_unit;
-
-architecture Behavioral of hazard_unit is
-
-begin
-
-
-end Behavioral;
+port (
+-- ulazni signali
+rs1_address_id_i : in std_logic_vector(4 downto 0);
+rs2_address_id_i : in std_logic_vector(4 downto 0);
+rs1_in_use_i : in std_logic;
+rs2_in_use_i : in std_logic;
+branch_id_i : in std_logic;
+rd_address_ex_i : in std_logic_vector(4 downto 0);
+mem_to_reg_ex_i : in std_logic;
+rd_we_ex_i : in std_logic;
+rd_address_mem_i : in std_logic_vector(4 downto 0);
+mem_to_reg_mem_i : in std_logic;
+-- izlazni kontrolni signali
+-- pc_en_o je signal dozvole rada za pc registar
+pc_en_o : out std_logic;
+-- if_id_en_o je signal dozvole rada za if/id registar
+if_id_en_o : out std_logic;
+-- control_pass_o kontrolise da li ce u execute fazu biti prosledjeni
+-- kontrolni signali iz ctrl_decoder-a ili sve nule
+control_pass_o : out std_logic
+);
+end entity;
