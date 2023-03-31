8f7136d8d1fb016799ff30d80d3e9a6a05d900f6
Djordje_Pavlovic <pavlovicdjordje35@gmail.com>
2023-03-31 17:31:30 +0200

    First imp of ALU. Added addition, subtraction, and gate, or gate.

--- RISC_V/RISC_V.srcs/sources_1/new/ALU.vhd
+++ RISC_V/RISC_V.srcs/sources_1/new/ALU.vhd
@@ -1,43 +1,34 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/31/2023 02:36:23 PM
--- Design Name: 
--- Module Name: ALU - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+use ieee.math_real.all;
+use work.alu_ops_pkg.all;
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity ALU is
---  Port ( );
-end ALU;
+ENTITY ALU IS
+GENERIC(
+WIDTH : NATURAL := 32);
+PORT(
+a_i : in STD_LOGIC_VECTOR(WIDTH-1 DOWNTO 0); --prvi operand
+b_i : in STD_LOGIC_VECTOR(WIDTH-1 DOWNTO 0); --drugi operand
+op_i : in STD_LOGIC_VECTOR(4 DOWNTO 0); --port za izbor operacije
+res_o : out STD_LOGIC_VECTOR(WIDTH-1 DOWNTO 0); --rezultat
+zero_o : out STD_LOGIC; -- signal da je rezultat nula
+of_o : out STD_LOGIC); -- signal da je doslo do prekoracenja opsega
+END ALU;
 
 architecture Behavioral of ALU is
 
+    signal add_r,sub_r,or_r,and_r,res_s : STD_LOGIC_VECTOR(WIDTH-1 downto 0);
+    
 begin
-
+    -- add
+    add_r <= std_logic_vector(unsigned(a_i) + unsigned(b_i));
+    -- sub
+    sub_r <= std_logic_vector(unsigned(a_i) - unsigned(b_i));
+    -- or
+    or_r <= a_i or b_i;
+    -- and
+    and_r <= a_i and b_i;
+    
 
 end Behavioral;
---
+++ RISC_V/RISC_V.srcs/sources_1/new/alu_ops_pkg.vhd
@@ -0,0 +1,13 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+package alu_ops_pkg is 
+    
+    type alu_op_t is (and_op,or_op,add_op,sub_op);
+    
+end package alu_ops_pkg;
+
+
+
